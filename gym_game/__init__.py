89f7fae0e630cd626c6f4907e61c4f6d22440a16
Babak Ansari-Jaberi <babakansari@yahoo.com>
2020-07-29 16:14:16 -0400

    [RL-3547] Give proper name to the environment ID as per specifications

--- gym_game/__init__.py
+++ gym_game/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='Pygame-babak',
+    id='Babak-v0',
     entry_point='gym_game.envs:CustomEnv',
     max_episode_steps=2000,
 )
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def simulate():
 
 
 if __name__ == "__main__":
-    env = gym.make("Pygame-babak")
+    env = gym.make("Babak-v0")
     MAX_EPISODES = 9999
     MAX_TRY = 1000
     epsilon = 1
